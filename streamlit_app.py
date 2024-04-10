004bdb85a94c39a3525407fa9787ee8cac187dd5
GScorp0611 <128770660+GScorp0611@users.noreply.github.com>
2024-04-10 11:20:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import streamlit as st
 
 st.title("Mời bạn nhập dữ liệu")
 st.header("Tên công ty")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
